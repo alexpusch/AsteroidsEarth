3c44fa3bc7d81e830d7ac8b33ae822fd4a1b908e
Alex <alex@local.host>
2013-11-09 21:40:50 +0200

    Added PIXI to grunt file

--- Gruntfile.js
+++ Gruntfile.js
@@ -94,13 +94,14 @@ module.exports = function (grunt) {
         },
         jasmine: {
             pivotal: {
-                src: ['.tmp/script/box2d.js', '.tmp/scripts/*.js'],
+                src: ['.tmp/scripts/*.js'],
                 options: {
                     specs: '.tmp/spec/*specs.js',
                     helpers: '.tmp/spec/*helper.js',
                     vendor: ['app/bower_components/jquery/jquery.js',
                              'app/bower_components/underscore/underscore.js',
-                             'app/bower_components/box2dweb/Box2dWeb-2.1.a.3.js'],
+                             'app/bower_components/box2dweb/Box2dWeb-2.1.a.3.js',
+                             'app/bower_components/pixi/bin/pixi.dev.js'],
                     template: require('grunt-template-jasmine-requirejs'),
                     templateOptions: {
                       requireConfigFile: 'config.js'
